4529902ba27bb15f08361a593cabcd4c93f1c111
PABG1990 <pedro19908454@gmail.com>
2025-06-04 18:20:46 -0500

    Ajuste

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -23,8 +23,8 @@
           .col-lg-12
             figure
               .video
-                iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-            figcaption Video. 
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/NzeNiyeMnAo?si=EEUPM8-ySncp1kGS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            figcaption Video. Normatividad y estructura del Sistema Nacional de Áreas Protegidas de Colombia SINAP
 
 </template>
 
